add419dc6c772f9b09fd48fc29beda29158a10f2
Guillaume Albert <58825251+gllmlbrt@users.noreply.github.com>
2026-08-17 19:56:48 +0200

    Update import for ConfigFlowResult compatibility
    Handle import compatibility for ConfigFlowResult based on Home Assistant version.

--- custom_components/heru/config_flow.py
+++ custom_components/heru/config_flow.py
@@ -5,8 +5,12 @@ from __future__ import annotations
 from typing import Any
 
 import voluptuous as vol
-from homeassistant.config_entries import ConfigFlow, ConfigFlowResult
-from homeassistant.const import CONF_HOST, CONF_NAME, CONF_PORT
+from homeassistant.config_entries import ConfigFlow
+
+try:
+    from homeassistant.config_entries import ConfigFlowResult
+except ImportError:  # HA < 2024.6 does not export ConfigFlowResult here
+    from homeassistant.data_entry_flow import FlowResult as ConfigFlowResultfrom homeassistant.const import CONF_HOST, CONF_NAME, CONF_PORT
 from pymodbus.client import AsyncModbusTcpClient
 
 from .const import DEFAULT_NAME, DEFAULT_PORT, DOMAIN
